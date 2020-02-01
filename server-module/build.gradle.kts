ecc60efcf3222efe031209416cd1d6a6d80b0446
Chie <jihye.kim@rakuten.com>
2020-02-01 16:34:41 +0900

    Add docker plugin in server-module

--- server-module/build.gradle.kts
+++ server-module/build.gradle.kts
@@ -5,13 +5,12 @@ repositories {
 plugins {
     id("net.ltgt.apt") version "0.21"
     id("net.ltgt.apt-idea") version "0.21"
-//    id("io.freefair.lombok") version "3.0.0"
 }
 
 apply {
     plugin("org.springframework.boot")
     plugin("io.spring.dependency-management")
-    plugin("com.bmuschko.docker-spring-boot-application")
+    plugin("io.freefair.lombok")
 }
 
 apply(from = "../versions.gradle.kts")
@@ -21,9 +20,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     implementation("com.h2database:h2:${project.extra["h2.version"]}")
-
-    implementation("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
 
     implementation("org.mapstruct:mapstruct:${project.extra["mapstruct.version"]}")
     annotationProcessor("org.mapstruct:mapstruct-processor:${project.extra["mapstruct.version"]}")
@@ -37,4 +33,17 @@ dependencies {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+val dockerImageName: String by extra { "spring-boot-example" }
+val dockerImageTag: String by extra { "0.0.1" }
+
+docker {
+    springBootApplication {
+        baseImage.set("openjdk:8-alpine")
+        maintainer.set("doutorking@gmail.com")
+        ports.set(listOf(8080))
+        images.set(setOf("$dockerImageName:$dockerImageTag"))
+        jvmArgs.set(listOf("-Dspring.profiles.active=dev", "-Xmx1024m"))
+    }
 }
